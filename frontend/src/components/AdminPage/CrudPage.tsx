9ecab578415525057e72755328fcac656dc61376
syerikkh <147494171+syerikkh@users.noreply.github.com>
2024-03-15 15:01:26 +0800

    home
    home

--- frontend/src/components/AdminPage/CrudPage.tsx
+++ frontend/src/components/AdminPage/CrudPage.tsx
@@ -1,31 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import { HeaderPart } from '../HeaderPart'
-import { FooterPart } from '../FooterPart'
-import { Category } from '../Category'
-import { CategoryContainer } from '../CategoryContainer'
-import { ThreeDotIcon } from '../Icons/ThreeDotIcon'
-import { FoodCategoryLabel } from '../FoodCategoryLabel'
-import { FoodsCard } from '../FoodsCard'
-import { CreateFoodCard } from '../CreateFoodCard'
-import { CreateCategoryCard } from '../CreateCategoryCard'
-import { GetStaticProps } from 'next'
-import axios from 'axios'
-import { any } from 'prop-types'
+import React, { useState } from 'react';
+import { HeaderPart } from '../HeaderPart';
+import { FoodCategoryLabel } from '../FoodCategoryLabel';
+import { CreateCategoryCard } from '../CreateCategoryCard';
+import { CreateFoodCard } from '../CreateFoodCard';
+import { GetServerSideProps } from 'next';
+import axios from 'axios';
 
+interface CategoryData {
+    id: string;
+    name: string;
+}
 
-export const CrudPage = () => {
+interface Props {
+    categoryData: CategoryData[];
+}
+
+const CrudPage = ({ categoryData }: Props) => {
     const [foodCardVisible, setFoodCardVisible] = useState(false);
     const [categoryCardVisible, setCategoryCardVisible] = useState(false);
-    const [categoryData, setCategoryData] = useState([]);
-
-
-    const fetchCategoryData = async () => {
-        const response = await axios.get("http://localhost:8000/category");
-        setCategoryData(response.data);
-    }
-    useEffect(() => {
-        fetchCategoryData();
-    }, [])
 
     return (
         <div>
@@ -36,29 +28,33 @@ export const CrudPage = () => {
                 <div className='flex flex-col gap-5'>
                     <h1 className='text-xl font-bold'>Food menu</h1>
                     <div className='flex flex-col gap-5'>
-                        {categoryData.map((category) => <FoodCategoryLabel name={category.name} />)}
+                        {categoryData.map((category) => <FoodCategoryLabel key={category.id} name={category.name} />)}
                     </div>
                     <button onClick={() => { setCategoryCardVisible(!categoryCardVisible) }} className='px-4 hover:bg-main text-[#5E6166] hover:text-white py-2 border w-[280px] h-[43px] rounded-lg font-medium cursor-pointer flex justify-between items-center'>
                         Create New Category
                     </button>
                 </div>
                 <div className='flex flex-col gap-5 w-full'>
-                    <div className='flex justify-between'>
-                        <h1 className='text-xl font-bold'>Breakfast</h1>
-                        <button onClick={() => { setFoodCardVisible(!foodCardVisible) }} className='p-2 bg-main text-white rounded-sm'>Add new food</button>
-                    </div>
-                    <div className='flex justify-between flex-wrap'>
-                        <FoodsCard foodImage={<img src='FoodPic.png' />} discount={20} foodName='haha' foodPrice={20} discountPrice={20} />
-                        <FoodsCard foodImage={<img src='FoodPic.png' />} discount={20} foodName='haha' foodPrice={20} discountPrice={20} />
-                        <FoodsCard foodImage={<img src='FoodPic.png' />} discount={20} foodName='haha' foodPrice={20} discountPrice={20} />
-                        <FoodsCard foodImage={<img src='FoodPic.png' />} discount={20} foodName='haha' foodPrice={20} discountPrice={20} />
-                        <FoodsCard foodImage={<img src='FoodPic.png' />} discount={20} foodName='haha' foodPrice={20} discountPrice={20} />
-                        <FoodsCard foodImage={<img src='FoodPic.png' />} discount={20} foodName='haha' foodPrice={20} discountPrice={20} />
-                        <FoodsCard foodImage={<img src='FoodPic.png' />} discount={20} foodName='haha' foodPrice={20} discountPrice={20} />
-                    </div>
+                    {/* Rest of your JSX */}
                 </div>
-
             </div>
         </div>
-    )
-}
+    );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    try {
+        const response = await axios.get("http://localhost:8000/category");
+        const categoryData: CategoryData[] = response.data;
+        return {
+            props: { categoryData },
+        };
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return {
+            props: { categoryData: [] },
+        };
+    }
+};
+
+export default CrudPage;
